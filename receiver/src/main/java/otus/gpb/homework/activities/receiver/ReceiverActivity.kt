8b2ee7221bfb5b39a66601307213cd7eb5e7a0bb
ArtemRem <art12rem@gmail.com>
2025-11-05 09:54:10 +0400

    Activity#2

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,8 @@
 package otus.gpb.homework.activities.receiver
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +10,27 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val title = findViewById<TextView>(R.id.titleTextView)
+            title.text = intent.getStringExtra("title").orEmpty()
+
+        val year = findViewById<TextView>(R.id.yearTextView)
+        year.text = intent.getStringExtra("year").orEmpty()
+
+        val description = findViewById<TextView>(R.id.descriptionTextView)
+        description.text = intent.getStringExtra("description").orEmpty()
+
+
+        val imageId = when (intent.getStringExtra("title").orEmpty()) {
+            "Славные парни" -> R.drawable.niceguys
+            "Интерстеллар" -> R.drawable.interstellar
+            else -> 0
+        }
+
+        findViewById<ImageView>(R.id.posterImageView).run {
+            setImageResource(imageId)
+            }
+
+
     }
 }
---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,62 @@
+package otus.gpb.homework.activities.sender
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.receiver.R
+
+class SenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sender)
+
+        findViewById<Button>(R.id.button_ToGoogleMaps).setOnClickListener {
+
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=restaurant"))
+                            .setPackage("com.google.android.apps.maps")
+            try { startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Не удалось открыть карты",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        findViewById<Button>(R.id.button_SendEmail).setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:android@otus.ru?subject=\"Письмо в otus \"&body=\"Домашнее задание activities_02.\""))
+                try { startActivity(intent)
+                } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Не удалось открыть почтовый агент",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        findViewById<Button>(R.id.button_OpenReceiver).setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.addCategory(Intent.CATEGORY_DEFAULT)
+            intent.type = "text/plain"
+            intent.putExtra("title", "Славные парни")
+            intent.putExtra("year", "2016")
+            intent.putExtra("description", "Что бывает, когда напарником брутального костолома становится субтильный лопух? Наемный охранник Джексон Хили ...")
+                try { startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Не удалось открыть Receiver",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+    }
+}
